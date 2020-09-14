957072d96e92af16060483faf3774a60263387f4
josue <josue0dev@gmail.com>
2020-09-14 23:59:30 +0100

    Updates

---
+++ precache-manifest.fcae652f1b282c7fe82caa8b949cfd26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6971e8f54ae811b7ba9970ee45cd7844",
+    "url": "/404-not-found/index.html"
+  },
+  {
+    "revision": "7561edbb63ffe59bb44a",
+    "url": "/404-not-found/static/css/main.48bb70ca.chunk.css"
+  },
+  {
+    "revision": "d39ce45688d54358f7d1",
+    "url": "/404-not-found/static/js/2.ab695456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/404-not-found/static/js/2.ab695456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7561edbb63ffe59bb44a",
+    "url": "/404-not-found/static/js/main.792ebd91.chunk.js"
+  },
+  {
+    "revision": "459f26064bba2836a41d",
+    "url": "/404-not-found/static/js/runtime-main.f487f5ec.js"
+  },
+  {
+    "revision": "9f720a55ebc8ddf98c2593757aaa959c",
+    "url": "/404-not-found/static/media/scarecrow.9f720a55.png"
+  }
+]);
